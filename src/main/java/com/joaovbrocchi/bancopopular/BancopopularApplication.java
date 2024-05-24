6e9baadac1270387b787e0f616a319b1934dcb76
joaovbrocchi <joaobrocchiprofissional@gmail.com>
2024-05-24 20:35:42 -0300

    first commit

---
+++ src/main/java/com/joaovbrocchi/bancopopular/BancopopularApplication.java
@@ -0,0 +1,13 @@
+package com.joaovbrocchi.bancopopular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancopopularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancopopularApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/joaovbrocchi/bancopopular/model/Conta.java
@@ -0,0 +1,4 @@
+package com.joaovbrocchi.bancopopular.model;
+
+public class Conta {
+}
